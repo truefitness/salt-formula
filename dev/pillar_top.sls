029f2d792d98f46f1075e1add6f396e27cbeb47a
Robert Hedges <rhedges@truefitness.com>
2016-04-04 17:04:41 -0500

    Add customization for truefitness.

--- dev/pillar_top.sls
+++ dev/pillar_top.sls
@@ -1,3 +1,6 @@
 base:
   'salt':
     - salt
+  'vim':
+    - vim
+
--- dev/setup-salt.sh
+++ dev/setup-salt.sh
@@ -7,7 +7,8 @@ sudo apt-get install salt-master -y
 sudo apt-get install salt-minion -y
 # setup top files to test the formula
 sudo mkdir -p /srv/pillar
-sudo ln -s /srv/salt/pillar.example /srv/pillar/salt.sls
+sudo ln -s /srv/salt/pillar.truefitness /srv/pillar/salt.sls
+#sudo ln -s /srv/salt/pillar.example /srv/pillar/salt.sls
 sudo ln -s /srv/salt/dev/pillar_top.sls /srv/pillar/top.sls
 # this file will be copied to make a running config. it should not be checked in.
 sudo cp /srv/salt/dev/state_top.sls /srv/salt/top.sls
